062bfc3ee5a39dcfe154a523b3cacf3e4da7272c
Caitlyn <craigoda+caitlyn@gmail.com>
2021-07-16 07:44:50 -0700

    finished first project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,10 @@ public class Main {
         mobilePhone.addNewContact(new Contact("Jim", "98765432"));
         mobilePhone.addNewContact(new Contact("Tom", "290290292"));
         mobilePhone.addNewContact(new Contact("Jim", "98765432"));
+
+mobilePhone.updateContact(new Contact("Jim", "98765432"), new Contact("Ben", "886728472"));
+mobilePhone.queryContact("Ben");
+mobilePhone.printContacts();
+//        mobilePhone.removeContact(new Contact("Tom", "290290292"));
     }
 }
--- src/com/company/MobilePhone.java
+++ src/com/company/MobilePhone.java
@@ -12,19 +12,67 @@ public class MobilePhone {
     }
 
     public boolean addNewContact(Contact newContact){
-        if(findContact(newContact) < 0){
+        System.out.println("Index = " + findContact(newContact.getName()));
+        if(findContact(newContact.getName()) >= 0){
+            System.out.println("Contact already exists");
+            return false;
+        }
+         else {
             myContacts.add(newContact);
             System.out.println(newContact.getName() + " was added with phone num " + newContact.getPhoneNumber());
             return true;
-        } else {
-            System.out.println("Contact already exists");
+        }
+    }
+
+    public boolean updateContact(Contact oldContact, Contact newContact){
+        if(findContact(oldContact.getName()) >= 0){
+            myContacts.set(findContact(oldContact.getName()), newContact);
+            System.out.println("ITEM REPLACED");
+            return true;
+        } else{
+            System.out.println("Contact does not exist");
             return false;
         }
     }
 
-    private int findContact(Contact findContact){
-        return this.myContacts.indexOf(findContact);
+    public boolean removeContact(Contact contact){
+        if(findContact(contact.getName()) >= 0){
+            myContacts.remove(contact);
+            System.out.println("Item " + contact.getName() + " was removed");
+            return true;
+        } else{
+            System.out.println("Item not found");
+            return false;
+        }
+    }
 
+    private int findContact(Contact contact){
+        return this.myContacts.indexOf(contact);
+    }
 
+    private int findContact(String newName){
+        for(int i = 0; i<myContacts.size(); i++){
+            Contact newContact = myContacts.get(i);
+            if(newContact.getName().equals(newName)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Contact queryContact(String contactName){
+        if(findContact(contactName) >= 0){
+            System.out.println("Returning " + myContacts.get(findContact(contactName)).getName());
+            return myContacts.get(findContact(contactName));
+
+        }else {
+            return null;
+        }
+    }
+
+    public void printContacts(){
+        for(int i = 0; i<myContacts.size(); i++){
+            System.out.println(myContacts.get(i).getName() + " -> " + myContacts.get(i).getPhoneNumber());
+        }
     }
 }
